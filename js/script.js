9f1606416560eef58ebb7a208730f9f8d8c010c5
shatyrbekova <perizat.shatyrbekova@gmail.com>
2021-06-15 15:41:15 +0200

    The third part has done.

--- js/script.js
+++ js/script.js
@@ -2,27 +2,20 @@
 //Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
  //Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell’oggetto.
 
- var datesListStudent ={
-     studentDates:[]
- }
+ var studentDates =
+
+  { nome:"Perizat", cognome: "Shatyrbekova", eta: 26}
+    
+console.log(studentDates);
 
 
-datesListStudent.studentDates.push({
-    name: 'Perizat',
-    surname: 'Shatyrbekova',
-    age: '27'
-})
-
-console.log(datesListStudent);
-
-
-for ( var key in datesListStudent){
-    console.log(key, datesListStudent[key])
+for ( var key in studentDates){
+    console.log(key, studentDates [key])
 }
 
 var studentInformations = document.getElementById('studenti');
 
-studentInformations.innerHTML= '<li>' + datesListStudent+ '</li>';
+studentInformations.innerHTML+= studentDates;
 
 //  PARTE 2
 // Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome. 
@@ -60,11 +53,11 @@ while (studentiBoolean.classmates.length <1){
 }
 
 
-console.log (studentiBoolean);
+console.log (studentiBoolean.classmates);
 
 for ( var y=0; y <studentiBoolean.classmates.length; y++){
 
-    for (var key in studentiBoolean[y]){
+    for (var key in studentiBoolean.classmates[y]){
         console.log(key, studentiBoolean.classmates[y][key])
     }
 
